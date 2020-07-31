207f57544e49c7d2658112633d77c7a6a4af1341
David Orson <onlyorson@outlook.com>
2020-07-31 07:23:03 +0100

    Home Page and MVP Site Scaffolding

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="ui container">
+      <h1>Code Quizzer</h1>
+      <button class="ui button">Start</button>
+    </div>
+  );
+};
+
+export default Home;
