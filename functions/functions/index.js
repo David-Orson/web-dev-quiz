16fb8200fb136e849bbef1213cf24a569d0945d4
David Orson <onlyorson@outlook.com>
2020-08-07 10:12:32 +0100

    Firebase Config

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -1,10 +1,7 @@
 const functions = require("firebase-functions");
-const admin = require("firebase-admin");
 const app = require("express")();
 
 const cors = require("cors");
 app.use(cors());
-
-admin.initializeApp();
 
 app.post("/signup", signup);
---
+++ functions/functions/utility/firebaseConfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyA7gOYUBHvkPAUq7Z3Wab8UuRtYQgKb0XU",
+  authDomain: "code-quizzer.firebaseapp.com",
+  databaseURL: "https://code-quizzer.firebaseio.com",
+  projectId: "code-quizzer",
+  storageBucket: "code-quizzer.appspot.com",
+  messagingSenderId: "81573663368",
+  appId: "1:81573663368:web:b444c10e6da642d7ebc53d",
+  measurementId: "G-4TJY7VVY8V",
+};
