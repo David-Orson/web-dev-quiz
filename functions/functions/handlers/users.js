c8a65811d1174cb1b3da0a2d44193dd06784f073
David Orson <onlyorson@outlook.com>
2020-08-07 11:13:47 +0100

    Signup User and Deploy (Signup + validator tested)

---
+++ functions/functions/handlers/users.js
@@ -0,0 +1,60 @@
+const { admin, db } = require("../utility/admin");
+
+const firebaseConfig = require("../utility/firebaseConfig");
+
+const firebase = require("firebase");
+firebase.initializeApp(firebaseConfig);
+
+const { validateSignupData } = require("../utility/validators");
+
+exports.signup = async (req, res) => {
+  try {
+    const newUser = {
+      email: req.body.email,
+      password: req.body.password,
+      confirmPassword: req.body.confirmPassword,
+      handle: req.body.handle,
+    };
+
+    const { valid, errors } = validateSignupData(newUser);
+
+    if (!valid) return res.status(400).json(errors);
+
+    let token, userId;
+    const doc = await db.doc(`/users/${newUser.handle}`).get();
+
+    const data = doc.exists
+      ? res.status(400).json({ handle: "This username is unavailable" })
+      : await firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password);
+
+    userId = data.user.uid;
+
+    const idToken = await data.user.getIdToken();
+
+    token = idToken;
+
+    const userCredentials = {
+      handle: newUser.handle,
+      email: newUser.email,
+      createdAt: new Date().toISOString(),
+      userId,
+    };
+
+    const done = await db.doc(`/users/${newUser.handle}`).set(userCredentials);
+
+    if (done) {
+      return res.status(201).json({ token });
+    }
+  } catch (err) {
+    console.error(err);
+    if (err.code === "auth/email-already-in-use") {
+      return res.status(400).json({ email: "Email is already in use " });
+    } else {
+      return res
+        .status(500)
+        .json({ general: "Something went wrong, please try again" });
+    }
+  }
+};
--- functions/functions/index.js
+++ functions/functions/index.js
@@ -4,4 +4,8 @@ const app = require("express")();
 const cors = require("cors");
 app.use(cors());
 
+const { signup } = require("./handlers/users");
+
 app.post("/signup", signup);
+
+exports.api = functions.region("europe-west1").https.onRequest(app);
