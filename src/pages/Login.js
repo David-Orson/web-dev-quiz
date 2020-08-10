07a3e37c40c145466f776e026aa242f9c48c41a4
David Orson <onlyorson@outlook.com>
2020-08-10 14:20:54 +0100

    Login Form Logic

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email);
+    console.log(password);
+  };
   return (
     <div className="ui raised very padded text container segment">
       <h2>Login</h2>
-      <form noValidate>
-        <div>
-          <label>Email Address</label>
-          <input />
-        </div>
-        <div>
-          <label>Password</label>
-          <input />
-        </div>
-      </form>
+
+      <div>
+        <label>Email Address</label>
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Password</label>
+        <input
+          type="text"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button className="ui button blue" onClick={handleSubmit}>
+        login
+      </button>
     </div>
   );
 }
